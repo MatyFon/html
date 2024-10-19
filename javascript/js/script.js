12e865dfcd3dc6498e8a8e1ee51dde6976477598
MatyFon <matiasquinones2@gmail.com>
2024-10-19 12:39:42 -0300

    prueba actualizada 2

---
+++ javascript/js/script.js
@@ -0,0 +1,5 @@
+/*Introducción*/
+//Comentario de linea
+    alert("Mi primera linea de JS"); /**Mensaje de alerta en pantalla**/
+    document.write("<h1>Mi primera linea de JS</h1><hr>"); /**Sirve para escribir en la página**/
+    console.log("Esto es un mensaje desde el script");
